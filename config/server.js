9176ed0343f7af023082026a89e86c79a889a5af
brennohv <brennovicentini@gmail.com>
2022-08-21 20:19:08 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c881f6004bb08c640f6223c28253948d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '24f7dfa8-92c3-4c02-9032-9892db9a15ef'
+};
