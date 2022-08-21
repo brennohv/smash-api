2036e5ab09f2d17145b99f81725d538d7c92e5d1
brennohv <brennovicentini@gmail.com>
2022-08-21 20:50:41 +0100

    update ssl

--- config/database.js
+++ config/database.js
@@ -1,18 +1,22 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'smashburger'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'brenin22'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "smashburger"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "brenin22"),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {}
+      options: {
+        ssl: true,
+      },
     },
   },
 });
