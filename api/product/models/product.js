04d0b31d0a3bd650757ce9f1ac6be2e05976107e
brennohv <brennovicentini@gmail.com>
2022-08-22 00:09:49 +0100

    add webHook

--- api/product/models/product.js
+++ api/product/models/product.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const axios = require("axios");
+const vercelWebhook = strapi.config.get("custom.vercelWebhook");
+module.exports = {
+  // const netlifyWebhook = strapi.config.get("custom.netlifyWebhook");
+
+  lifecycles: {
+    async afterCreate(_, __) {
+      netlifyWebhook && axios.post(vercelWebhook);
+    },
+    async afterUpdate(_, __, ___) {
+      netlifyWebhook && axios.post(vercelWebhook);
+    },
+  },
+};
---
+++ config/env/production/custom.js
@@ -0,0 +1,4 @@
+module.exports = () => ({
+  vercelWebhook:
+    "https://api.vercel.com/v1/integrations/deploy/prj_geZkijBwnAOeLd7pxyl6v2BIvnuF/MVNGIPvW2F",
+});
